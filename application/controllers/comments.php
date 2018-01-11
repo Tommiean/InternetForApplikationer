458e098534aca00d3fac90014a713001b41d2c3f
Tommiean <tommiea_96@hotmail.com>
2018-01-11 17:10:02 +0100

    Update comments.php

--- application/controllers/comments.php
+++ application/controllers/comments.php
@@ -1,23 +1,24 @@
 <?php
 class comments extends CI_Controller{
-    public function create(){
-        $page = $this->input->post('page');
-        $this->form_validation->set_rules('body', 'Comment', 'required');
-        $data['comments'] = $this->Comment_model->get_comments();
-        if($this->form_validation->run() === FALSE){
-            $this->load->view('templates/header');
-            $this->load->view('pages/'.$page, $data);
-            $this->load->view('templates/footer');
-        }else{
-            $this->Comment_model->create_comment($page);
-            $this->session->set_flashdata('comment_posted', 'Your comment has been posted!');
-            redirect($page);
-        }
+
+    public function showComments(){
+        $result = $this->Comment_model->showComments();
+        echo json_encode($result);
     }
-    public function delete($id){
-        $page = $this->input->post('page');
-        $this->Comment_model->delete_comment($id);
-        $this->session->set_flashdata('comment_deleted', 'Your comment has been deleted!');
-        redirect($page);
+    public function addComment(){
+        $result = $this->Comment_model->addComment();
+        $msg['success'] = false;
+        if($result){
+            $msg['success'] = true;
+        }
+        echo json_encode($msg);
+    }
+        public function delete(){
+        $result = $this->Comment_model->delete_comment();
+        $msg['success'] = false;
+        if($result){
+            $msg['success'] = true;
+        }
+        echo json_encode($msg);
     }
 }
